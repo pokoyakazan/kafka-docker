c9dfc1e4b283fef1c5212b607139a476d62f0aa3
pokoyakazan <okuyama1625@gmail.com>
2021-10-05 09:46:49 +0900

    Add message for partition control with key

--- scripts/producer.py
+++ scripts/producer.py
@@ -13,14 +13,19 @@ def main():
     # producer の設定
     producer = KafkaProducer(
         bootstrap_servers=['localhost:{}'.format(port)],  # Kafka Broker ホスト
-        value_serializer=lambda x: dumps(x).encode('utf-8')  # シリアライズ
+        value_serializer=lambda x: dumps(x).encode('utf-8'),  # シリアライズ
+        acks=1 # リーダーパーティションへの書き込みを待って送信官僚とする
     )
 
     # トピックにメッセージを送信
     for j in range(1000):
         message = {'testProduceData': j}
-        # Python ではライブラリがトピックを自動生成する
+        # Python ではライブラリがトピックを自動生成する(第一引数がトピック名)
         producer.send('topicPython', value=message)  # トピック名と送信メッセージを指定
+
+        # 以下のようにkeyを指定して配置先のパーティションをコントロールすることが可能
+        # producer.send('topicPython', key='hostname', value=message)
+
         print(message)
         sleep(0.5)
 
